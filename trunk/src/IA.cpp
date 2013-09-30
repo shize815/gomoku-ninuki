21d5674ff2032a7d48f29d35181385975a640adc
tisserant@gmail.com <tisserant@gmail.com@5d27bbe7-948b-00e3-f4c4-26c1b6b15df9>
2013-09-30 12:14:53 +0000

    IA.cpp

---
+++ trunk/src/IA.cpp
@@ -0,0 +1,96 @@
+#include "../include/IA.h"
+
+
+IA::IA()
+{
+    //ctor
+}
+
+void IA::setjeu(jeu * jt){
+    j = jt;
+}
+
+IA::~IA()
+{
+    //dtor
+}
+
+
+
+bool IA::coupIntelligent(coup * c){ //TODO!
+    //Ordi a 4 pions alignés :
+    /*
+    for(int x=0; x<plateau.size(); x++){
+        for(int y=0; y<plateau.size(); y++){
+            if(plateau()){
+
+            }
+        }
+
+    }
+    */
+    return false;
+
+}
+
+
+bool IA::coupRand(coup * c){
+
+    int c1= rand()%3-1+j->getcx1();
+    int c2= rand()%3-1+j->getcy1();
+    if(c1<0){
+        c1=0;
+    }
+    if(c1>15){
+        c1=15;
+    }
+    if(c2<0){
+        c2=0;
+    }
+    if(c2>15){
+        c2=15;
+    }
+    if(j->getplateau(c1,c2)==0){
+       //plateau[c1][c2]=2;
+    } else {
+        while(j->getplateau(c1,c2)!=0){
+            c1+=rand()%3-1;
+            c2+=rand()%3-1;
+            if(c1<0){
+                c1=0;
+            }
+            if(c1>15){
+                c1=15;
+            }
+            if(c2<0){
+                c2=0;
+            }
+            if(c2>15){
+                c2=15;
+            }
+        }
+        //plateau[c1][c2]=2;
+    }
+    /*
+    cx=c1;
+    cy=c2;
+    cc=2;
+    */
+    //coup retour;
+    c->x=c1;
+    c->y=c2;
+    return true;
+
+}
+
+
+void IA::repond(){
+
+    coup reponse;
+    if(coupIntelligent(&reponse)){
+
+    } else {
+        coupRand(&reponse);
+    }
+    j->joue2(reponse.x, reponse.y);
+}
