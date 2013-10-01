f36fa5e33e42c1a89edb67c0441656daa1bbc425
cordonnier.etienne <cordonnier.etienne@gmail.com>
2013-10-01 22:02:37 +0000

    Rend nombre de joueurs configurable (humain vs humain ou humain vs IA). Supprime fichiers générés par CodeBlock à chaque compilation (.layout et .depend)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 
 #include "./include/jeu.h"
 #include "./include/IA.h"
+#include <cstring>
 
-
-
-int main(){
+void joueurContreIA(){
     Coup coup;
     Jeu jeu;
     IA ia;
@@ -17,36 +16,46 @@ int main(){
         cin>>coup.y;
         cout<<"            x :" << endl;
         cin>>coup.x;
-        if (jeu.coupJoueur1(coup) != SUCCES) {
+        if (jeu.coupJoueur1(coup) != SUCCES){
             cout << "coup invalide" << endl;
             continue;
         }
-        //jeu.regles();
         jeu.affiche();
         ia.repond();
-        //jeu.regles();
     }
-/*
-    while(!j.fin()){
-        //cout<<"plateau : "<<endl;
+}
+
+void joueurContreJoueur(){
+    Coup coup;
+    Jeu jeu;
+
+    while(!jeu.fin()){
         jeu.affiche();
         cout<<"coup joueur 1 :"<<endl;
-        cin>>coup1;
-        cin>>coup2;
-        jeu.coupJoueur1(coup1,coup2);
-        jeu.regles();
+        cin>>coup.y;
+        cin>>coup.x;
+        jeu.coupJoueur1(coup);
+
         jeu.affiche();
-
         cout<<"coup joueur 2 :"<<endl;
-        cin>>coup1;
-        cin>>coup2;
-        jeu.coupJoueur2(coup1,coup2);
-
-
-        //j.repond();
-        jeu.regles();
+        cin>>coup.y;
+        cin>>coup.x;
+        jeu.coupJoueur2(coup);
     }
-*/
-    return 0;
+}
 
+int main(){
+    int nombreJoueurs = 0;
+    cout << "Entrez 1 pour jouer contre l'IA, 2 pour jouer contre un humain." << endl;
+
+    cin>>nombreJoueurs; /* TODO: sécuriser les saisies, au cas où quelqu'un tape des lettres par exemple */
+    if (nombreJoueurs == 1) {
+        //"mode 1 joueur"
+        joueurContreIA();
+    } else if (nombreJoueurs == 2) {
+        // "mode 2 joueurs"
+        joueurContreJoueur();
+    }
+
+    return 0;
 }
