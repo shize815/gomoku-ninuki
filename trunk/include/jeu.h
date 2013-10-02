258a380b44d767fd054203e2f88ed263b7b548c0
cordonnier.etienne@gmail.com <cordonnier.etienne@gmail.com@5d27bbe7-948b-00e3-f4c4-26c1b6b15df9>
2013-10-02 21:34:09 +0000

    Empêche les coups invalides en mode 2 joueurs
    Corrige bug mineur qui faisait que l'IA pouvait jouer des coups aléatoires invalides (x ou y = 15 alors que le plateau va de 0 à 14)

--- trunk/include/jeu.h
+++ trunk/include/jeu.h
@@ -3,6 +3,9 @@
 #include <vector>
 #include <iostream>
 
+#define HAUTEUR_PLATEAU 15
+#define LARGEUR_PLATEAU 15
+
 struct Coup{
     int x;
     int y;
@@ -16,7 +19,7 @@ enum Resultat {
 
 
 //Jeu contient tout ce qui a attrait au jeu (la résolution des prises, la détection d'un gagnant...)
-//La classe conserve toutes les informations liés au plateau.
+//La classe conserve toutes les informations liées au plateau.
 
 class Jeu
 {
@@ -25,37 +28,33 @@ class Jeu
         virtual ~Jeu();
         void affiche(); //Affiche le plateau dans le terminal.
         Resultat jouerCoup(Coup coup);
-
+        int oppose(int couleur); //retourne la couleur inverse à la couleur entrée
         bool fin(); //renvoie vrai si la partie est finie.
 
+        //ACCESSEURS :
         int getp1();
         int getp2();
-
-        //ACCESSEURS :
         int getplateau(int x, int y);
         std::vector<Coup> &getcoupsJoues();
 
     protected:
     private:
-        void regles(Coup dernierCoup); //Applique les règles du jeu.
-        int oppose(int couleur); //retourne la couleur inverse à la couleur entrée
+        void regles(); //Applique les règles du jeu.
+
 
         //renvoie le nombre de pions de la couleur donné aligné dans la direction (d1,d2) een partant de la case (x,y)
         int lireligne(int x, int y, int d1, int d2, int couleur);
 
         std::vector<Coup> coupsJoues;
 
-
-
         std::vector < std::vector <int> > plateau;
-
 
         //Numero du joueur ayant fait le dernier coup
         int joueurDernierCoup;
 
         int p1; //prisonniers du j1
         int p2;
-        int v; //0 si la partie est en cours, sinon le numero de vainqueur.
+        int joueurVictorieux; //0 si la partie est en cours, sinon le numero de vainqueur.
 };
 
 #endif // JEU_H
